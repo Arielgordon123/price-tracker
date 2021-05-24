51c37992610915c7eca6ad1768ae150938993d2f
Ariel Gordon <arielgordon123@gmail.com>
2021-05-24 08:29:38 +0300

    add store types

--- src/stores/store.entity.ts
+++ src/stores/store.entity.ts
@@ -8,9 +8,25 @@ import {
   ObjectIdColumn,
 } from 'typeorm';
 import { ObjectID } from 'mongodb';
+import { IStore, storeApiProviders } from './store.interface';
 
 @Entity()
-export class storeEntity extends BaseEntity {
+export class storeEntity extends BaseEntity implements IStore {
+  subChainId?: string;
+  storeId?: string;
+  bikoretNo?: string;
+  storeType?: string;
+  chainName?: string;
+  subChainname?: string;
+  storeName?: string;
+  address?: string;
+  city?: string;
+  zipCode?: string | number;
+  latitude?: number;
+  longitude?: number;
+
+  chainName_en?: string;
+  provider: storeApiProviders;
   @ObjectIdColumn()
   id: ObjectID;
 
