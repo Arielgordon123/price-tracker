342d882ee7199893e0e6b504b977849b2d635f2b
Ariel Gordon <arielgordon123@gmail.com>
2021-05-24 18:51:15 +0300

    Create  Store interface

---
+++ src/stores/store.interface.ts
@@ -0,0 +1,33 @@
+import { ObjectID } from 'mongodb';
+
+export enum storeApiProviders {
+  'NIBIT' = 'nibit',
+  'PRIVATE' = 'private',
+  'CERBERUS' = 'cerberus',
+}
+
+export interface IStore {
+  id?: ObjectID;
+  ChainId?: string;
+  ChainName?: string;
+  XmlDocVersion?: string;
+  LastUpdateDate?: string;
+  LastUpdateTime?: string;
+  LastUpdateDateTime?: Date;
+  subChainId?: string;
+  storeId?: string;
+  bikoretNo?: string;
+  storeType?: string;
+  chainName?: string;
+  subChainname?: string;
+  storeName?: string;
+  address?: string;
+  city?: string;
+  zipCode?: string | number;
+
+  latitude?: number;
+  longitude?: number;
+  addressFull?: string;
+  chainName_en?: string;
+  provider: storeApiProviders;
+}
